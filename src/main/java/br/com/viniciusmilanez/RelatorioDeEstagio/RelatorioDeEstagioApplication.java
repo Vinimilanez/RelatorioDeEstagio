def5dd1bdd9f9be26d12afcab30a00a277b960ca
vinic <vinic@192.168.1.12>
2018-12-13 13:05:04 -0200

    Inicio do projeto.

---
+++ src/main/java/br/com/viniciusmilanez/RelatorioDeEstagio/RelatorioDeEstagioApplication.java
@@ -0,0 +1,12 @@
+package br.com.viniciusmilanez.RelatorioDeEstagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioDeEstagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatorioDeEstagioApplication.class, args);
+	}
+}
